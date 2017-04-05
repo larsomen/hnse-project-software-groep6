9840e3f058c74ca88b2b7f2aecbc38890bc33e6d
wmagdelyns <wmagdelyns@outlook.com>
2017-04-05 12:01:46 +0200

    pogin1

---
+++ testmaster/testmaster/main.c
@@ -0,0 +1,307 @@
+/*
+ * test2.c
+ *
+ * Created: 3/20/2016 6:09:06 PM
+ * Author : john
+ */ 
+
+#define F_CPU 16000000
+#define SCL_frequentie 100000
+#define BAUDRATE		9600
+#define UBBR 103 //207 for asynchronous communication
+//#define UBRR_BAUD	(((long)F_CPU/((long)16 * BAUDRATE))-1)
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <util/twi.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+
+#define DEVICE_ADRES   8
+#define RP6 DEVICE_ADRES
+#define KOMPAS 0x60
+
+void verzenden(uint8_t ad,uint8_t b);
+void ontvangen(uint8_t ad,uint8_t[],uint8_t);
+void init_master();
+
+void initUSART();
+void writeChar(char c);
+void writeInteger(int16_t number, uint8_t base);
+void writeString(char *string);
+
+void Input(char in);
+int ping();
+void turnTo(int hoek);
+int leesKompas();
+char *windrichting(int hoek);
+void gaNaarHoekR(int hoek);
+void gaNaarHoekL(int hoek);
+
+int main(void)
+{
+	PORTD = 0x03; //pullup SDA en SCL
+	initUSART();
+    init_master();
+	sei();
+
+	//gaNaarHoekR(90);
+	while (1)
+	{
+		verzenden(RP6, 30);
+		_delay_ms(100);
+	}
+
+}
+
+void init_master() {
+	TWSR = 0;
+	// Set bit rate
+	TWBR = ( ( F_CPU / SCL_frequentie ) - 16) / 2;
+	TWCR = (1<<TWEN);
+}
+
+void ontvangen(uint8_t ad,uint8_t b[],uint8_t max) {
+	uint8_t op[15];
+	
+	TWCR |= (1<<TWSTA);
+	while(!(TWCR & (1<<TWINT)));
+	op[0] = TWSR;
+
+	TWDR=(ad<<1)+1;
+	TWCR=(1<<TWINT)|(1<<TWEN);
+	while(!(TWCR & (1<<TWINT)));
+
+	op[1] = TWSR;
+	b[0]=TWDR;
+	
+	uint8_t tel=0;
+	do{
+		if(tel == max-1)
+		TWCR=(1<<TWINT)|(1<<TWEN);
+		else
+		TWCR=(1<<TWINT)|(1<<TWEN)|(1<<TWEA);
+		while(!(TWCR & (1<<TWINT)));
+		op[tel] = TWSR;
+		b[tel]=TWDR;
+	}while(op[tel++] == 0x50);
+
+	TWCR=(1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
+
+	//   for(uint8_t i=0;i<tel;++i) {
+	//	 writeString("\n\r");writeInteger(op[i],16);
+	//	 writeString(" data ");writeInteger(b[i],10);
+	//   }
+
+}
+
+void verzenden(uint8_t ad,uint8_t b) {
+	//  uint8_t op[5];
+
+	TWCR |= (1<<TWSTA);
+	while(!(TWCR & (1<<TWINT)));
+	//   op[0] = TWSR;
+	TWDR=(ad<<1);
+	TWCR=(1<<TWINT)|(1<<TWEN);
+	while(!(TWCR & (1<<TWINT)));
+	//    op[1] = TWSR;
+
+	TWDR=257;
+	TWCR=(1<<TWINT)|(1<<TWEN);
+	while(!(TWCR & (1<<TWINT)));
+	//  op[2] = TWSR;
+
+
+	TWCR=(1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
+	//	while(!(TWCR & (1<<TWINT)));
+	//  for(uint8_t i=0;i<3;++i) {
+	// writeString("\n\r");writeInteger(op[0],16);
+	// writeString(" ");writeInteger(op[1],16);
+	// writeString(" ");writeInteger(op[2],16);
+}
+
+
+void initUSART() {
+	UBRR0H = (unsigned char)(UBBR>>8);
+	UBRR0L = (unsigned char)UBBR;
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);	/* Enable receiver and transmitter */
+	UCSR0C = (1<<UCSZ00)|(1<<UCSZ01); /* Set frame format: 8data, 2stop bit */
+}
+
+void writeChar(char ch)
+{
+	while (!(UCSR0A & (1<<UDRE0)));
+	UDR0 = (uint8_t)ch;
+}
+
+void writeString(char *string)
+{
+	while(*string)
+	writeChar(*string++);
+}
+
+void writeInteger(int16_t number, uint8_t base)
+{
+	char buffer[17];
+	itoa(number, &buffer[0], base);
+	writeString(&buffer[0]);
+}
+
+ISR(USART0_RX_vect){
+	Input(UDR0);
+}
+
+void Input(char in){
+	switch(in){
+		case 'w':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 1);
+		break;
+		case 's':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 2);
+		break;
+		case 'a':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 3);
+		break;
+		case 'd':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 4);
+		break;
+		case '+':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 5);
+		break;
+		case '-':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 6);
+		break;
+		case 'q':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 7);
+		break;
+		case 'e':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 8);
+		break;
+		case '0':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		verzenden(DEVICE_ADRES, 0);
+		break;
+		case '4':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		gaNaarHoekL(180);
+		break;
+		case '6':
+		writeChar('\t');writeChar(in);writeChar('\r');
+		gaNaarHoekR(180);
+		break;
+	}
+}
+
+int ping(){
+	//mega pin 48
+	int x = 0;
+	DDRL = 0xFF;     // all output
+	_delay_us(2);
+	PORTL = (1 << PINL1);  //pin PL1 (48 on arduino) high
+	_delay_us(5);    //delay of 5 us
+	PORTL = ~(1 << PINL1);  //4th pin low
+	DDRL = 0x00;     // all input
+	//wacht op pin input van ultra
+	while (!(PINL & (1 << PINL1))){
+		_delay_us(0.5);
+	}
+	//ga tellen
+	while (PINL & (1 << PINL1)){
+		x++;
+		_delay_us(0.5);
+	}
+	//us naar cm een cm is 29us een gaat hen en weer
+	x = x / 58;
+	return x;
+}
+
+int leesKompas(){
+	uint16_t hoek;
+	uint8_t b[10];
+	verzenden(0x60, 2);
+	ontvangen(0x60, b, 2);
+	hoek = ((b[0]<<8)+b[1])/10;
+	return hoek;
+}
+
+char *windrichting(int hoek){
+	char *r;
+	if (hoek < 45)
+	{
+		r = "Noord";
+	}
+	else if (hoek >= 45 && hoek <135)
+	{
+		r = "Oost";
+	}
+	else if (hoek>=135 && hoek <225){
+		r ="Zuid";
+	}
+	else if (hoek>= 225 && hoek<315){
+		r = "West";
+	}
+	else if(hoek >= 315){
+		r = "Noord";
+	}
+	else{
+		r = "ERR";
+	}
+
+	return r;
+}
+
+void gaNaarHoekR(int hoek){
+	int huidig = leesKompas();
+	int bestem = huidig + hoek;
+	if (bestem < 359)
+	{
+		while(leesKompas() < bestem){
+			verzenden(RP6, 14);
+			_delay_ms(10);
+		}
+		verzenden(RP6, 0);
+	}
+	else{
+		while(leesKompas() > (bestem -360)){
+			verzenden(RP6, 14);
+			_delay_ms(10);
+		}
+		while(leesKompas() < (bestem-360)){
+			verzenden(RP6, 14);
+			_delay_ms(10);
+		}
+		verzenden(RP6, 0);
+	}
+}
+
+void gaNaarHoekL(int hoek){
+	int huidig = leesKompas();
+	int bestem = huidig - hoek;
+	if (bestem > 0)
+	{
+		while(leesKompas() > bestem){
+			verzenden(RP6, 13);
+			_delay_ms(10);
+		}
+		verzenden(RP6, 0);
+	}
+	else{
+		while(leesKompas() < (bestem+360)){
+			verzenden(RP6, 13);
+			_delay_ms(10);
+		}
+		while(leesKompas() > (bestem +360)){
+			verzenden(RP6, 13);
+			_delay_ms(10);
+		}
+		verzenden(RP6, 0);
+	}
+}
---
+++ testslave/testslave/main.c
@@ -0,0 +1,380 @@
+/*
+ * i2cw.c
+ *
+ * Created: 3/19/2016 2:14:44 PM
+ * Author : john
+ */ 
+
+ #define F_CPU 8000000
+ #define SCL_frequentie 100000
+ #define BAUDRATE		38400
+ #define UBRR_BAUD	(((long)F_CPU/((long)16 * BAUDRATE))-1)
+ #define resetData()  for(uint8_t i=0;i<20;++i) data[i]=0
+ #define rset() for(uint8_t i=0;i<20;++i) data_ont[i]=0
+
+ #define UBAT 			(1 << PINA7) // ADC7 (Input)
+ #define MCURRENT_L 		(1 << PINA6) // ADC6 (Input)
+ #define MCURRENT_R 		(1 << PINA5) // ADC5 (Input)
+ #define E_INT1 			(1 << PINA4) // INT1 (input per default... can be output)
+ #define LS_L 			(1 << PINA3) // ADC3 (Input)
+ #define LS_R 			(1 << PINA2) // ADC2 (Input)
+ #define ADC1 			(1 << PINA1) // ADC1 (Input)
+ #define ADC0 			(1 << PINA0) // ADC0 (Input)
+
+ #define SL4 		(1 << PINB7)	// Output
+ #define ACS_L 		(1 << PINB6)	// Output
+ #define START 		(1 << PINB5)	// Input
+ #define PWRON		(1 << PINB4)	// Output
+ #define ACS_PWRH	(1 << PINB3)	// Output
+ #define ACS 		(1 << PINB2)	// INT2 (Input)
+ #define SL5 		(1 << PINB1)	// Output
+ #define SL6 		(1 << PINB0)	// Output
+
+
+ #define ACS_R 		(1 << PINC7)	// Output
+ #define SL3 		(1 << PINC6)	// Output
+ #define SL2 		(1 << PINC5)	// Output
+ #define SL1 		(1 << PINC4)	// Output
+ #define DIR_R 		(1 << PINC3)	// Output
+ #define DIR_L 		(1 << PINC2)	// Output
+ #define SDA 		(1 << PINC1)	// I2C Data (I/O)
+ #define SCL 		(1 << PINC0)	// I2C Clock (Output (Master), Input (Slave))
+
+
+ #define ACS_PWR		(1 << PIND6)	// Output
+ #define MOTOR_R		(1 << PIND5)	// PWM Output (OC1A)
+ #define MOTOR_L		(1 << PIND4)	// PWM Output (OC1B)
+ #define ENC_R 		(1 << PIND3)	// INT1 (Input)
+ #define ENC_L 		(1 << PIND2)	// INT0 (Input)
+ #define TX 		(1 << PIND1)	// USART TX (Output)
+ #define RX 		(1 << PIND0)	// USART RX (Input)
+
+#include <avr/io.h>
+#include <util/twi.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <util/delay.h>
+
+#define TRUE 0xFF;
+#define FALSE 0;
+
+#define MaxSpeed 150
+#define MinSpeed 0
+
+void init_i2c_slave(uint8_t ad);
+void init_i2c_ontvang( void (*ontvanger) (uint8_t [],uint8_t));
+void init_i2c_verzend( uint8_t (*verzender) ());
+void slaaftwi();
+void (*ontfunc) (uint8_t[],uint8_t);
+uint8_t (*verfunc) ();
+
+void initUSART();
+void writeInteger(int16_t number, uint8_t base);
+void writeString(char *string);
+void writeChar(char ch);
+
+void motorIint(void);
+void control(int inByte);
+void act(uint8_t speedL, uint8_t speedR, uint8_t links, uint8_t rechts);
+
+
+ISR(TWI_vect) {
+
+	slaaftwi();
+
+}
+
+uint8_t data_ont[20]; //max 20
+volatile uint8_t data_flag = FALSE;
+volatile uint8_t databyte=0x33;
+
+void ontvangData(uint8_t [],uint8_t);
+uint8_t verzendByte(uint8_t x);
+
+uint16_t encoder_L_count;
+uint16_t encoder_R_count;
+void initEncoders(void);
+int encoder_Count_to_Centimeter(int encoder_count);
+
+uint16_t encoder_L_driven;
+uint16_t encoder_R_driven;
+uint16_t encoder_L_total;
+uint16_t encoder_R_total;
+
+
+int main(void)
+{
+
+	//DDRC=0xFF;
+	initUSART();
+	init_i2c_slave(8);
+	
+	/*ontvangData is de functie die uitgevoerd wordt 
+	wanneer een byte via de i2c bus ontvangen wordt
+	*/
+	init_i2c_ontvang(ontvangData); 
+	
+	/*verzendByte is de functie die aangeroepen wordt
+	wanneer de slave een byte naar de master verzend*/
+	init_i2c_verzend(verzendByte);
+
+	motorIint();
+	initEncoders();
+	sei(); //De slave van i2c werkt met interrupt
+	
+    /* Replace with your application code */
+	int drivenL;
+    while (1) 
+    {  
+		//control(data_ont[0]);
+		//data_ont[0] = 255;
+		//_delay_ms(10);
+		uint16_t hoek;
+
+		writeInteger(data_ont[0],10);
+		writeChar('\t');
+		writeInteger(data_ont[1],10);
+		writeChar('\t');
+		hoek = ((data_ont[1]<<8)+data_ont[0]);
+		writeInteger(hoek,10);
+		writeString("\r\n");
+
+
+    }
+	
+}
+ /*slave heeft data ontvangen van de master
+ data[] een array waarin de ontvangen data staat
+ tel het aantal bytes dat ontvangen is*/
+ 
+void ontvangData(uint8_t data[],uint8_t tel){
+	for(int i=0;i<tel;++i)
+	    data_ont[i]=data[i];
+	data_flag = TRUE;
+	//writeString("o\n\r");
+}
+
+/* het byte dat de slave verzend naar de master
+in dit voorbeeld een eenvoudige teller
+*/
+
+uint8_t verzendByte(uint8_t x) {
+		return x;
+}
+
+void init_i2c_slave(uint8_t ad) {
+	
+	TWSR = 0;
+	TWBR = ((F_CPU / SCL_frequentie) - 16) / 2;
+	TWCR = (1 << TWIE) | (1 << TWEN) | (1 << TWEA);
+	TWAR = ad<<1;
+}
+
+void slaaftwi() {
+	static uint8_t data[40];
+	static uint8_t teller=0;
+	switch(TWSR) {
+		case 0x10:
+		case 0x08:
+		break;
+		
+		case 0x60:
+
+		teller=0;
+
+		break;
+		case 0x68:
+
+		break;
+		case 0x80:
+		data[teller++] = TWDR;
+		break;
+
+		case 0xA0:
+		ontfunc(data,teller);
+		resetData();
+		break;
+
+		case 0xA8:
+		teller=0;
+		TWDR=verfunc();
+		break;
+
+		case 0xB8:
+		TWDR=verfunc();
+		break;
+
+		case 0xC0:   //NACK
+		break;
+		case 0xC8:
+		break;
+	}
+	TWCR |= (1<<TWINT);    // Clear TWINT Flag
+}
+
+void init_i2c_ontvang( void (*ontvanger) (uint8_t [],uint8_t)) {
+	ontfunc=ontvanger;
+}
+
+void init_i2c_verzend( uint8_t (*verzender) ()) {
+	verfunc=verzender;
+}
+
+void initUSART() {
+
+	UBRRH = UBRR_BAUD >> 8;
+	UBRRL = (uint8_t) UBRR_BAUD;
+	UCSRA = 0x00;
+	UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);
+	UCSRB = (1 << TXEN) | (1 << RXEN);
+	writeString("usart werkt nog\n\r");
+}
+
+void writeChar(char ch)
+{
+	while (!(UCSRA & (1<<UDRE)));
+	UDR = (uint8_t)ch;
+}
+
+void writeString(char *string)
+{
+	while(*string)
+	writeChar(*string++);
+}
+
+void writeInteger(int16_t number, uint8_t base)
+{
+	char buffer[17];
+	itoa(number, &buffer[0], base);
+	writeString(&buffer[0]);
+}
+
+void motorIint(void){
+	DDRC |= (1<<2)|(1<<3);
+
+	TCCR1A = (1<<COM1A1)|(1<<COM1B1)|(1<<WGM10);
+	ICR1 = 210; //is max in plaats van 255 (volgens datashets)
+	OCR1A = 0;
+	OCR1B = 0;
+	//start timers
+	TCCR1B = (1<<WGM12)|(1<<CS10);
+}
+
+void control(int inByte){
+	static uint8_t speedL = 0, speedR = 0;
+	static uint8_t directionL = 0, directionR = 0;
+	if(inByte == 1){
+		directionL = 0;
+		directionR = 0;
+		speedL = speedR;
+	}
+	if (inByte == 2){
+		directionL = 1;
+		directionR = 1;
+		speedL = speedR;
+	}
+
+	if(inByte == 3){
+		directionL = 1;
+		directionR = 0;
+	}
+
+	if(inByte == 4){
+		directionR = 1;
+		directionL = 0;
+	}
+
+	if (inByte == 5){
+		if(speedL < MaxSpeed){
+			speedL += 10;
+		}
+		if(speedR < MaxSpeed){
+			speedR += 10;
+		}
+	}
+
+	if (inByte == 6){
+		if(speedL > MinSpeed){
+			speedL -= 10;
+		}
+		if(speedR > MinSpeed){
+			speedR -= 10;
+		}
+	}
+
+	if(inByte == 7){
+		if(speedR < MaxSpeed){
+			speedR += 10;
+		}
+		else if(speedL > MinSpeed){
+			speedL -= 10;
+		}
+	}
+
+	if(inByte == 8){
+		if(speedL < MaxSpeed){
+			speedL += 10;
+		}
+		else if(speedR > MinSpeed){
+			speedR -= 10;
+		}
+	}
+
+	if(inByte == 13){
+		directionL = 1;
+		directionR = 0;
+		speedL = 75;
+		speedR = 75;
+	}
+
+	if(inByte == 14){
+		directionL = 0;
+		directionR = 1;
+		speedL = 75;
+		speedR = 75;
+	}
+
+	if(inByte == 0){
+		directionL = 0;
+		directionR = 0;
+		speedL = 0;
+		speedR = 0;
+	}
+	act(speedL, speedR, directionL, directionR);
+}
+
+void act(uint8_t speedL, uint8_t speedR, uint8_t links, uint8_t rechts){
+	PORTC = (links<<2)|(rechts<<3);
+	OCR1A = speedR;
+	OCR1B = speedL;
+}
+
+void initEncoders(void){
+	// Enable/Disable Encoders, IR Receiver
+	DDRB |= (1 << 4);
+	PORTB |= (1 << PINB4);
+	//zet input van sensor
+	DDRD |= (0<<2)|(0<<3);
+	// Initialize External interrupts:
+	MCUCR |= (1<<ISC00) | (1<<ISC10);
+	GICR |= (1 << INT1) | (1 << INT0);
+	encoder_L_count = 0;
+	encoder_R_count = 0;
+}
+
+//enc_L
+ISR(INT0_vect)
+{
+	encoder_L_count++;
+	encoder_L_driven++;
+}
+//enc_R
+ISR(INT1_vect)
+{
+	encoder_R_count++;
+}
+
+int encoder_Count_to_Centimeter(int encoder_count){
+	//0.25 voor milimeters 0.025 voor centimeters
+	return (encoder_count * 0.025);
+}
